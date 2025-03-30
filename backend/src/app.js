b5a6fea5f07472f4086b701a1fc47eb6c6f945ff
Yaxin Yuan <yaxinyuan0910@gmail.com>
2025-03-30 17:07:48 -0400

    feat: enhance logging and update start script for backend server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
+const fs = require('fs');
+const path = require('path');
 const express = require("express");
-const mongoose = require("mongoose");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, '..', 'logs', 'access.log'),
+    { flags: 'a' }
+  );
 const http  = require("http");
 const cookieParser = require("cookie-parser");
 
 const app = express();
 const server = http.createServer(app);
+app.use(morgan('combined', { stream: accessLogStream }));
 
 mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/media-collection?authSource=admin`,
     {
@@ -20,10 +27,9 @@ const db = mongoose.connection;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello2 World!");
 });
 
 db.once("open", () => {
